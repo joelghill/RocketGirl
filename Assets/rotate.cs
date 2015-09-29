5cbd49fa86081b1d9ce9acf464d7da673d53d67e
Joel Hill <joel.hill.87@gmail.com>
2015-09-29 12:46:25 -0600

    Added prototype rotation script

---
+++ Assets/rotate.cs
@@ -0,0 +1,23 @@
+﻿/**
+ * Prototype rotation script
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	public GameObject level;
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonDown(1)) {
+			level.transform.Rotate(new Vector3(0,90,0));
+			player.transform.Rotate(new Vector3(0,-90,0));
+		}
+	}
+}
