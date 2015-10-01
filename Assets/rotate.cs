8005b9350d0b7e65ebf6346818fe5b647e5f1958
Joel Hill <joel.hill.87@gmail.com>
2015-10-01 14:20:19 -0600

    Updated raycast scene and source file. Script now moves collider over nearest platform.

--- Assets/rotate.cs
+++ Assets/rotate.cs
@@ -1,23 +1,57 @@
 ﻿/**
+ * Joel Hill 
  * Prototype rotation script
  */
 
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using Interpolation;
 
 public class rotate : MonoBehaviour {
 
 	public GameObject level;
-	public GameObject player;
+	//public double TransitionTime;
+	public GameObject[] entities;
+	public Camera camera;
+	public double cameraMovementY;
+
+	public float[] rotationPoints;
+
+	bool transitionFlag = false;
+	int currenttPoint = 0;
+	
 	// Use this for initialization
 	void Start () {
-	
+		//initialize dictionaries
+		level = this.gameObject;
+
+		rotationPoints = new float[4];
+		rotationPoints [0] = level.transform.rotation.y;
+		rotationPoints [1] = level.transform.rotation.y+90;
+		rotationPoints [2] = level.transform.rotation.y+180;
+		rotationPoints [3] = level.transform.rotation.y+270;
 	}
 
 	void Update () {
-		if (Input.GetMouseButtonDown(1)) {
-			level.transform.Rotate(new Vector3(0,90,0));
-			player.transform.Rotate(new Vector3(0,-90,0));
+		if (Input.GetMouseButton (0)) {
+			Quaternion newRotation 		= Quaternion.AngleAxis(getRotationGoal(), Vector3.up);
+			level.transform.rotation	= Quaternion.Slerp(transform.rotation, newRotation, .1f);
 		}
+	}
+
+	Quaternion CloneQuat(Quaternion q){
+		Quaternion n = new Quaternion(q.x, q.y, q.z, q.w);
+		return n;
+	}
+
+	float getRotationGoal(){
+		if (currenttPoint == 3)
+			return rotationPoints [0];
+		return rotationPoints [currenttPoint];
+	}
+
+	bool hasReachedGoal(){
+		return Mathf.Abs ((level.transform.rotation.y - getRotationGoal ())) <= 0.001;
 	}
 }
--- Assets/sendRaycast.cs
+++ Assets/sendRaycast.cs
@@ -2,28 +2,56 @@
 using System.Collections;
 
 public class sendRaycast : MonoBehaviour {
+	
 	public BoxCollider collider;
 	public Transform transform;
+	public Rigidbody rigid;
 	// Use this for initialization
 	void Start () {
-	
+		if (transform == null) {
+			transform = this.gameObject.transform;
+		}
+		if (this.collider == null)
+			this.collider = this.gameObject.GetComponent<BoxCollider> ();
+		if (rigid == null)
+			this.rigid = this.gameObject.GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (transform == null)
+		if (transform == null || this.rigid.velocity.magnitude == 0) {
+			Debug.Log ("No movement or transform.");
 			return;
+		}
+		if(isFloating())
+			Debug.Log ("FLOATING");
 		RaycastHit hit;
 		float distanceToGround = 0;
 		Vector3 pos = new Vector3 (transform.position.x, transform.position.y - 1, transform.position.z);
-		if (Physics.Raycast(pos, transform.forward, out hit)) {
+		if (Physics.Raycast (pos, transform.forward, out hit)) {
 			distanceToGround = hit.distance;
-			this.collider.size = (new Vector3(1, 1, (distanceToGround +1)*2));
-			Debug.Log(hit.ToString());
-			Debug.Log("Object is " + distanceToGround.ToString() + " away");
-		}else{
-			this.collider.size = (new Vector3(1, 1, 1));
+			this.collider.size = (new Vector3 (1, 1, (distanceToGround + 1) * 2));
+			return;
 		}
+			//Debug.Log(hit.ToString());
+			//Debug.Log("Object is " + distanceToGround.ToString() + " away");
+		if (Physics.Raycast (pos, -1*transform.forward, out hit)) {
+			distanceToGround = hit.distance;
+			this.collider.size = (new Vector3 (1, 1, (distanceToGround + 1) * 2));
+			return;
+		}
+		this.collider.size = (new Vector3(1, 1, 1));
+	}
 
+	bool isFloating(){
+		RaycastHit hit;
+		return !(Physics.Raycast (this.transform.position, new Vector3 (0, -1, 0),out hit ,1.0f));
+	}
+
+	void OnCollisionEnter(Collision c){
+		if (isFloating()) {
+			Vector3 pos = this.transform.position;
+			this.transform.position = new Vector3(pos.x, pos.y, c.gameObject.transform.position.z);
+		}
 	}
 }
