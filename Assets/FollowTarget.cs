59dc5c2bc24825fafd81f666b2d49d8e63327bf0
Joel Hill <joel.hill.87@gmail.com>
2015-10-06 11:19:27 -0600

    Added very basic camera follow player script. .bar file is for blackberry. Ignore.

---
+++ Assets/FollowTarget.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTarget : MonoBehaviour {
+
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target != null) {
+			this.transform.position= new Vector3(this.target.transform.position.x, this.target.transform.position.y, this.transform.position.z);
+		} else {
+			Debug.Log("CAMERA IS NULL");
+		}
+	}
+}
