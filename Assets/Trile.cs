bda0cd6595018e44c70da837adfbc599223d5f00
Joel Hill <joel.hill.87@gmail.com>
2015-10-09 09:52:58 -0600

    All basic blocks are now of type "Trile". Created editor scripts to generate a trile  from a sprite, or a set of triles from a sprite sheet. Assumes all sprites have a pixels per unit thta match their dimensions.

--- Assets/Trile.cs
+++ Assets/Trile.cs
@@ -13,7 +13,38 @@ public class Trile : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		
+		SpriteRenderer sp = this.Top.GetComponent<SpriteRenderer> ();
+		sp.receiveShadows = true;
+		sp.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+
+		sp = this.Bottom.GetComponent<SpriteRenderer> ();
+		sp.receiveShadows = true;
+		sp.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+
+		sp = this.Back.GetComponent<SpriteRenderer> ();
+		sp.receiveShadows = true;
+		sp.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+
+		sp = this.Front.GetComponent<SpriteRenderer> ();
+		sp.receiveShadows = true;
+		sp.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+
+		sp = this.Left.GetComponent<SpriteRenderer> ();
+		sp.receiveShadows = true;
+		sp.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+
+		sp = this.Right.GetComponent<SpriteRenderer> ();
+		sp.receiveShadows = true;
+		sp.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+
+	}
+	public void setAllSides(Sprite s){
+		this.SetTop (s);
+		this.SetRight (s);
+		this.SetLeft (s);
+		this.SetFront (s);
+		this.SetBack (s);
+		this.SetBottom (s);
 	}
 	
 	// Update is called once per frame
@@ -21,34 +52,36 @@ public class Trile : MonoBehaviour {
 		
 	}
 	public void SetTop(Sprite s){
-		Sprite sp = this.Top.GetComponent<Sprite> ();
+		SpriteRenderer sp = this.Top.GetComponent<SpriteRenderer> ();
+		Debug.Log("SetTop Called");
 		if (sp != null)
-			sp = s;
+			Debug.Log("SetTop Sprite Component not null");
+			sp.sprite = s;
 	}
 	public void SetBottom(Sprite s){
-		Sprite sp = this.Bottom.GetComponent<Sprite> ();
+		SpriteRenderer sp = this.Bottom.GetComponent<SpriteRenderer> ();
 		if (sp != null)
-			sp = s;
+			sp.sprite = s;
 	}
 	public void SetBack(Sprite s){
-		Sprite sp = this.Back.GetComponent<Sprite> ();
+		SpriteRenderer sp = this.Back.GetComponent<SpriteRenderer> ();
 		if (sp != null)
-			sp = s;
+			sp.sprite = s;
 	}
 	public void SetFront(Sprite s){
-		Sprite sp = this.Front.GetComponent<Sprite> ();
+		SpriteRenderer sp = this.Front.GetComponent<SpriteRenderer> ();
 		if (sp != null)
-			sp = s;
+			sp.sprite = s;
 	}
 	public void SetLeft(Sprite s){
-		Sprite sp = this.Left.GetComponent<Sprite> ();
+		SpriteRenderer sp = this.Left.GetComponent<SpriteRenderer> ();
 		if (sp != null)
-			sp = s;
+			sp.sprite = s;
 	}
 	public void SetRight(Sprite s){
-		Sprite sp = this.Right.GetComponent<Sprite> ();
+		SpriteRenderer sp = this.Right.GetComponent<SpriteRenderer> ();
 		if (sp != null)
-			sp = s;
+			sp.sprite = s;
 	}
 }
 
