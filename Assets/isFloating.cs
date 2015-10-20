7eb5ac33f2c43f87824917cc152e4b9d951c4230
Joel Hill <joel.hill.87@gmail.com>
2015-10-20 11:15:30 -0600

    Addition of "isFloating" script.

---
+++ Assets/isFloating.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// Is floating.
+/// 
+/// checks if game object is directly above another object.
+/// If not, moves in Z direction to position over nearest object.
+/// </summary>
+/// 
+public class isFloating : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
