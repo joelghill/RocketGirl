dc4895fc808576f8f5e85499ec0405a1d27122ad
Joel Hill <joel.hill.87@gmail.com>
2015-10-27 10:02:23 -0600

    Created A bullet object and added to test scene. Collides with player and removes health.

--- Assets/bullet.cs
+++ Assets/bullet.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class bullet : MonoBehaviour , IKillable {
     public float speed = 5;
     bool hostile = false;
+    public float power = 10;
     Rigidbody rb;
 	// Use this for initialization
 	void Start () {
@@ -12,12 +13,19 @@ public class bullet : MonoBehaviour , IKillable {
         //put bullet close to camera
         Vector3 pos = this.transform.position;
         this.transform.position = new Vector3(pos.x, pos.y, Camera.main.transform.position.z + 1);
+        setVelocity(speed, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    
+        onHit();
 	}
+
+    public void setVelocity(float speed, float rotation)
+    {
+        rb.velocity = new Vector3(speed, 0, 0);
+        rb.transform.Rotate(0, 0, rotation);
+    }
 
     public void makeHostile(bool value)
     {
@@ -27,5 +35,22 @@ public class bullet : MonoBehaviour , IKillable {
     public void Die()
     {
         Destroy(this.gameObject);
+    }
+
+    void onHit()
+    {
+        Vector3 pos = this.transform.position;
+        Vector3 hitCheck = new Vector3(pos.x, pos.y, Camera.main.transform.position.z);
+        RaycastHit hit;
+
+        if (Physics.Raycast(hitCheck, Camera.main.transform.forward, out hit))
+        {
+            IDamageable<float> damageable = hit.collider.gameObject.GetComponent<IDamageable<float>>();
+            if (damageable != null)
+            {
+                damageable.takeDamage(power);
+                Die();
+            }
+        }
     }
 }
