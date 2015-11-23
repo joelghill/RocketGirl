6ec4e25c0af242210ff0eafea3693880d196cf65
Carter Hill <carterghill@gmail.com>
2015-11-23 17:53:03 -0600

    Press escape to pause. No GUI for pause yet

---
+++ Assets/Pause.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	private bool isPause;
+
+	// Use this for initialization
+	void Start () {
+		isPause = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if( Input.GetKeyDown(KeyCode.Escape))
+		{
+			isPause = !isPause;
+			if(isPause)
+				Time.timeScale = 0;
+			else
+				Time.timeScale = 1;
+		}
+
+	}
+
+}
