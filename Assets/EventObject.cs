37a8917469d34d9a7d06f75255f09eca36e9144c
Joel Hill <joel.hill.87@gmail.com>
2015-11-24 13:59:34 -0600

    Created new scripts and prefabs for displaying signs and notifications

---
+++ Assets/EventObject.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EventObject : MonoBehaviour, IEvent {
+
+    public MessageBoard message;
+    public BoxCollider2D eventCollider;
+
+	// Use this for initialization
+	void Start () {
+        if(eventCollider == null)
+        {
+            eventCollider = gameObject.GetComponent<BoxCollider2D>();
+        }
+        if(eventCollider != null && !eventCollider.isTrigger)
+        {
+            eventCollider.isTrigger = true;
+        }
+
+        if(message != null)
+        {
+            message.enabled = false;
+        }
+
+        //adjust position of message board
+        Vector3 pos = transform.position;
+        message.transform.position = new Vector3(pos.x, pos.y + 2, Camera.main.transform.position.z +5);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            onSpriteCollisionEnter(other.gameObject);
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            onSpriteCollisionExit(other.gameObject);
+        }
+
+    }
+
+    public void onSpriteCollisionEnter(GameObject other)
+    {
+        Debug.Log("SHOW MESSAGE");
+        message.show();
+        //throw new NotImplementedException();
+    }
+
+    public void onSpriteCollisionExit(GameObject other)
+    {
+        //throw new NotImplementedException();
+        message.hide();
+        Debug.Log("HIDE MESSAGE");
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
