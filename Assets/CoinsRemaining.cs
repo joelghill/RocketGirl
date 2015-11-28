1615fef4d73a1357d0e0e23b951417f9d2bb6b82
Carter Hill <carterghill@gmail.com>
2015-11-27 19:00:45 -0600

    Added coins and UI that keeps track of amounts collected

---
+++ Assets/CoinsRemaining.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CoinsRemaining : MonoBehaviour {
+
+	private Text text;
+	private int coins;
+	private int totalCoins;
+
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text> ();
+		coins = 0;
+		totalCoins = GameObject.FindGameObjectsWithTag ("Coin").Length;
+	}
+
+	public void addCoin(){
+		coins = coins + 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = coins.ToString () + " / " + totalCoins.ToString ();
+	}
+}
---
+++ Assets/CollectCoin.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectCoin : MonoBehaviour {
+
+	public bool enabled = true;
+	private Collider col;
+	private CoinsRemaining coin;
+
+	// Use this for initialization
+	void Start () {
+		col = GetComponent<Collider> ();
+		col.enabled = enabled;
+		coin = GameObject.Find ("Coins Remaining").GetComponent<CoinsRemaining> ();
+	}
+
+	void OnTriggerEnter() {
+		Destroy (gameObject, 0.0f);
+		coin.addCoin ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Fire1") || Input.GetButtonDown ("Fire2")) {
+			col.enabled = !col.enabled;
+		}
+	
+	}
+}
