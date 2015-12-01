410b0a9f58418f0ede665d038605d4fa80ec27ca
Carter Hill <carterghill@gmail.com>
2015-12-01 09:58:56 -0600

    Alpha buildddddd

--- Assets/Scripts/Level/CoinsRemaining.cs
+++ Assets/Scripts/Level/CoinsRemaining.cs
@@ -5,12 +5,14 @@ using System.Collections;
 public class CoinsRemaining : MonoBehaviour {
 
 	private Text text;
+	public Text winText;
 	private int coins;
 	private int totalCoins;
 	private AudioSource win;
 	private AudioSource[] levelMusic;
 	private bool soundPlayed;
 	private Avatar playerAva;
+	private bool gameWon;
 
 	// Use this for initialization
 	void Start () {
@@ -21,14 +23,27 @@ public class CoinsRemaining : MonoBehaviour {
 		soundPlayed = false;
 		playerAva = GameObject.FindGameObjectWithTag ("Player").GetComponent<Avatar> ();
 		levelMusic = GameObject.Find ("level").GetComponents<AudioSource> ();
+		gameWon = false;
 	}
 
 	public void addCoin(){
 		coins = coins + 1;
 	}
-	
+
+	void OnGUI(){
+		if (gameWon && winText != null) {
+			GUI.Box (new Rect ((Screen.width) / 2 - (Screen.width) / 8,
+		                   (Screen.height) / 2 - (Screen.height) / 8, (Screen.width) / 4,
+		                   (Screen.height) / 4), winText.text);
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
+
+		if(gameWon && !win.isPlaying && (Input.GetButtonDown("Jump") || Input.GetButtonDown("Cancel") || Input.GetButtonDown("Submit"))){
+			Application.LoadLevel(0);
+		}
 
 		if (totalCoins > 0) {
 
@@ -48,10 +63,17 @@ public class CoinsRemaining : MonoBehaviour {
 
 			} else if (coins == totalCoins && soundPlayed && !win.isPlaying) {
 				int currentLevel = Application.loadedLevel;
-				Application.LoadLevel(currentLevel+1);
+				Debug.Log(Application.levelCount);
+				if(Application.levelCount - 1 > currentLevel){
+					Application.LoadLevel(currentLevel+1);
+				} else {
+					gameWon = true;
+				}
 			}
+
+
 		} else {
-			text.text = "No Coins In Level";
+			text.text = "No Coins";
 		}
 	}
 }
