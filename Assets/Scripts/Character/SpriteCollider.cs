15b96fc5298705ee9792c733fb244193ace17b57
Carter Hill <carterghill@gmail.com>
2015-12-05 14:24:39 -0600

    Added a tiny bit of height to the spawn point, spawning too close too the floor sometimes caused fall through

--- Assets/Scripts/Character/SpriteCollider.cs
+++ Assets/Scripts/Character/SpriteCollider.cs
@@ -68,7 +68,7 @@ public class SpriteCollider : MonoBehaviour {
                 else{
                     if(other.GetComponent<Player>() != null){
 						other.GetComponent<Player>().setRespawnPosition(new Vector3(transform.position.x,
-					                                                            this.transform.position.y+otherHeight, 
+					                                                            this.transform.position.y+otherHeight+0.5f, 
 					                                                            this.transform.position.z));
 					}
 
