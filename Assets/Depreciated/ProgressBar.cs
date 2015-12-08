6dd7d39f9e3dbf7683b32d9a9d41814cb1688715
Vanessa Wiwchar <vanessa.wiwchar@usask.ca>
2015-12-08 10:19:30 -0600

    more stuff

---
+++ Assets/Depreciated/ProgressBar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ProgressBar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Image image = GetComponent<Image>();
+        image.fillAmount = 1f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Image image = GetComponent<Image>();
+        image.fillAmount = .7f;
+        //image.fillAmount = Mathf.MoveTowards(image.fillAmount, 1f, Time.deltaTime * .5f);
+	}
+}
---
+++ Assets/Depreciated/timer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class timer : MonoBehaviour {
+    public Text counterText;
+    public float seconds, minutes;
+
+    // Use this for initialization
+    void Start()
+    {
+        counterText = GetComponent<Text>() as Text;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        minutes = (int)(Time.time / 60f);
+        seconds = (int)(Time.time % 60f);
+        counterText.text = minutes.ToString("02") + ":" + seconds.ToString("00");
+    }
+
+    //Time.timeSinceLevelLoad
+
+    /**void onResetClick()
+    {
+       Button resetButton = GetComponent<Button>();
+       minutes = (int)(Time.time / 60f);
+       seconds = (int)(Time.time % 60f);
+       counterText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+    }**/
+   
+}
